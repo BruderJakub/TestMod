7093fc576d55ba14974c6e1fb2f1638fca796c54
BruderJakub <jakub.pilecki@stud.kbw.ch>
2026-03-04 09:55:38 +0100

    add Recipes and Loot Tables

--- src/main/java/ch/megamodermaster/testmod/block/ModBlocks.java
+++ src/main/java/ch/megamodermaster/testmod/block/ModBlocks.java
@@ -2,9 +2,11 @@ package ch.megamodermaster.testmod.block;
 
 import ch.megamodermaster.testmod.TestMod;
 import ch.megamodermaster.testmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,12 +23,14 @@ public class ModBlocks {
     public static final RegistryObject<Block> POKER_CHIP_BLOCK = registerBlock("poker_chip_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+
     public static final RegistryObject<Block> CHIP_BLOCK = registerBlock("chip_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+            () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops()));
+
     public static final RegistryObject<Block> CREEPIER_POKER_CHIP_BLOCK = registerBlock("creepier_poker_chip_block",
-            () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
+            () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of()
+                    .strength(5f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
